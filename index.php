8f5ccbec90cc62889c6d2ad8e9380ed3502cd79a
Dara Anggi Puspa <daraanggip19@gmail.com>
2025-02-16 13:05:57 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
     <h1>
         Halo halo
 </h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam placeat minus odio, quos, laborum soluta accusamus tempora ratione qui odit distinctio velit corporis ipsum, veniam est at voluptatibus nulla laboriosam?
+</p>
 </body>
 </html>
